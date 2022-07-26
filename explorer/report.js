304ef6331721c156dae29b81612f129c3395628e
hughes-ch <contact@chrishughesdev.com>
2022-07-26 14:57:11 -0400

    Cleaning up TransferReport

--- explorer/report.js
+++ explorer/report.js
@@ -12,15 +12,7 @@ class TransferReport {
    * @param {Array[Transaction]} transactionList - An array of Transactions
    */
   constructor(transactionList) {
-    const fieldNames = [
-      'totalEtherTransfer',
-      'receivingAddresses',
-    ]
-
-    for (const name of fieldNames) {
-      const camelCaseName = name[0].toUpperCase() + name.slice(1)
-      this[name] = this[`get${camelCaseName}`](transactionList)
-    }
+    this.transactionList = transactionList
   }
 
   /** Converts the object to an ASCII representation */
@@ -28,24 +20,20 @@ class TransferReport {
     return `Total Ether Transfer: ${this.totalEtherTransfer}`
   }
 
-  /**
-   * Gets the total amount of ether transferred in the transactionList
-   * @param {Array[Transaction]} transactionList - An array of Transactions
-   */
-  getTotalEtherTransfer(transactionList) {
-    return transactionList.reduce(this.reduceTransactions, Web3.utils.toBN(0))
+  /** Gets the total amount of ether transferred */
+  get totalEtherTransfer() {
+    return this.transactionList.reduce(
+      this.reduceTransactions, Web3.utils.toBN(0)
+    )
   }
 
-  /**
-   * Gets the receiving addresses (and how much they received)
-   * @param {Array[Transaction]} transactionList - An array of Transactions
-   */
-  getReceivingAddresses(transactionList) {
-    const addresses = transactionList.map(tx => tx.to)
+  /** Gets the receiving addresses and how much they received */
+  get receivingAddresses() {
+    const addresses = this.transactionList.map(tx => tx.to)
     const uniqueAddresses = Array.from(new Set(addresses))
     const receivingAddresses = {}
     for (const address of uniqueAddresses) {
-      const txToAddress = transactionList.filter(tx => tx.to === address)
+      const txToAddress = this.transactionList.filter(tx => tx.to === address)
       receivingAddresses[address] = txToAddress.reduce(
         this.reduceTransactions, Web3.utils.toBN(0)
       )
